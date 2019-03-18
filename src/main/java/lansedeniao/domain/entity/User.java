bb6424ebd7737fd68f52b51eab6f4dc8f2c381b2
emilg1101 <emilyandro99@gmail.com>
2019-03-18 19:52:00 +0800

    User domain entity

--- src/main/java/lansedeniao/domain/entity/User.java
+++ src/main/java/lansedeniao/domain/entity/User.java
@@ -2,41 +2,82 @@ package lansedeniao.domain.entity;
 
 import lansedeniao.data.entity.UserDto;
 
+import java.time.Instant;
+import java.util.Date;
+
 public class User {
 
+    private long id;
     private String username;
     private String password;
+    private String name;
+    private String email;
+    private Date registrationDate;
+    private Date birthDate;
+    private String country;
+    private int gender;
+    private int likesCount;
+    private int postsCount;
 
-    public User(String username, String password) {
+    public User(long id, String username, String password, String name, String email, Date registrationDate, Date birthDate, String country, int gender, int likesCount, int postsCount) {
+        this.id = id;
         this.username = username;
         this.password = password;
+        this.name = name;
+        this.email = email;
+        this.registrationDate = registrationDate;
+        this.birthDate = birthDate;
+        this.country = country;
+        this.gender = gender;
+        this.likesCount = likesCount;
+        this.postsCount = postsCount;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public String getName() {
+        return name;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public int getPostsCount() {
+        return postsCount;
     }
 
     public static User mapper(UserDto from) {
-        return new User("username", "password");
+        return new User(0, "username", "password", "", "", Date.from(Instant.now()), Date.from(Instant.now()), "", 2, 0, 0);
     }
 }
