9746bf6f5df7f96a0b8ceb2f346c752a9de524d3
emilg1101 <emilyandro99@gmail.com>
2019-03-25 21:01:12 +0800

    update database dump

--- dump/lansedeniao_new_schema_post.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO new_schema.post (id, user_id, text, cdate) VALUES (1, 1, 'My first post!!!', '2019-03-21 13:10:49.636925');
-INSERT INTO new_schema.post (id, user_id, text, cdate) VALUES (2, 1, 'My 2nd post.', '2019-03-21 15:49:17.680557');
-INSERT INTO new_schema.post (id, user_id, text, cdate) VALUES (4, 3, 'test', '2019-03-22 00:08:35.490726');
